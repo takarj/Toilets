31b39aef0e625d2e94f9c970745906c2095eb1e2
takarj <tkrjagd@gmail.com>
2018-06-30 11:04:44 +0200

    bottomsheetV1

--- app/src/main/java/de/wdgpocking/lorenz/toilets/MainActivity.java
+++ app/src/main/java/de/wdgpocking/lorenz/toilets/MainActivity.java
@@ -4,17 +4,20 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.support.design.widget.BottomSheetBehavior;
-import com.google.android.gms.maps.CameraUpdateFactory;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener, GoogleMap.OnMapClickListener {
 
     private GoogleMap map;
     private BottomSheetBehavior sheetBehavior;
+    private boolean hud;
+    private static final int PEEK_HEIGHT_COLLAPSED = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +28,12 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        hud = true;
         View bottomSheet = findViewById(R.id.bottom_sheet1);
-        sheetBehavior = BottomSheetBehavior.from
+        sheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        sheetBehavior.setPeekHeight(PEEK_HEIGHT_COLLAPSED);
+
     }
 
     /**
@@ -54,7 +61,27 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapLongClick(LatLng latLng) {
         //TODO
-        MarkerOptions mOpt = new MarkerOptions();
+        Toast.makeText(this, "LONG CLICK", Toast.LENGTH_SHORT);
+        MarkerOptions mOpt = new MarkerOptions()
+                .position(latLng)
+                .title("Custom Toilet");
         map.addMarker(mOpt);
+    }
+
+
+    /**
+     * @param latLng
+     * Hides or Unhides HUD (BottomSheet + SearchBar)
+     */
+    @Override
+    public void onMapClick(LatLng latLng) {
+        //TODO
+        Toast.makeText(this, "CLICK", Toast.LENGTH_SHORT);
+        if(hud){
+            sheetBehavior.setPeekHeight(0);
+        }else {
+            sheetBehavior.setPeekHeight(PEEK_HEIGHT_COLLAPSED);
+            sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        }
     }
 }
