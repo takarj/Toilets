33a65d7c123cf7bd6c131c1f8c2af1bbae6ddb54
takarj <tkrjagd@gmail.com>
2018-12-17 14:32:55 +0100

    toilets

--- app/src/main/java/de/wdgpocking/lorenz/toilets/MainActivity.java
+++ app/src/main/java/de/wdgpocking/lorenz/toilets/MainActivity.java
@@ -37,8 +37,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import org.json.JSONObject;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
